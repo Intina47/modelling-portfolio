75cf94cdb614389de9ca3e842dbb0f03c4da51a3
uzohou <isaiahntina47@gmail.com>
2024-01-04 17:02:46 +0000

    Update MyLifeStory component layout

--- app/components/MyLifeStory.tsx
+++ app/components/MyLifeStory.tsx
@@ -1,10 +1,11 @@
 // components/MyLifeStory.tsx
+'use client';
 import React from 'react';
 import Image from 'next/image';
 
 const MyLifeStory = () => {
   return (
-    <section className="py-16 bg-white">
+    <section className="py-16 ">
       <div className="container mx-auto flex flex-col items-center">
         {/* Numbered Title */}
         <div className="flex items-center mb-6">
@@ -25,21 +26,21 @@ const MyLifeStory = () => {
         </div>
         {/* mobile */}
           <div className="md:hidden flex flex-col md:flex-row items-center md:justify-between">
-          <div className="md:hidden relative mb-4 md:mb-0 md:w-1/3 rounded-lg">
-          <Image src="/mylife1.jpg" alt="Photo 1" width="400" height="600" className="mb-4 md:mb-0 md:w-1/3 rounded-lg object-cover" />
+          <div className="md:hidden relative mb-4 md:mb-0 md:w-1/3">
+          <Image src="/mylife1.jpg" alt="Photo 1" width="400" height="600" className="mb-4 md:mb-0 md:w-1/3 mx-1 rounded-l-lg object-cover" />
             <div className="md:hidden absolute bottom-0 right-0 p-2 bg-black text-white opacity-75">
               Photo: Balenciaga Walk 2020
             </div>
           </div>
-          <div className="md:hidden relative mb-4 md:mb-0 md:w-1/3 rounded-lg">
-          <Image src="/mylife2.jpg" alt="Photo 2" width="400" height="600" className="mb-4 md:mb-0 md:w-1/3 rounded-lg" />
+          <div className="md:hidden relative mb-4 md:mb-0 md:w-1/3">
+          <Image src="/mylife2.jpg" alt="Photo 2" width="400" height="600" className="mb-4 md:mb-0 md:w-1/3 mx-1 rounded-l-lg" />
             <div className="md:hidden absolute bottom-0 right-0 p-2 bg-black text-white opacity-75">
               Photo: Vogue Photoshoot
             </div>
           </div>
 
-           <div className="md:hidden relative md:w-1/3 rounded-lg">
-           <Image src="/mylife3.jpg" alt="Photo 3" width="400" height="600" className="md:w-1/3 rounded-lg" />
+           <div className="md:hidden relative md:w-1/3 bg-green">
+           <Image src="/mylife3.jpg" alt="Photo 3" width="400" height="600" className="md:w-1/3 mx-1 rounded-l-lg" />
             <div className="md:hidden absolute bottom-0 right-0 p-2 bg-black text-white opacity-75">
               Photo: Runway Show Paris
             </div>
