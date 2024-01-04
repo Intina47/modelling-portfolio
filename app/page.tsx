4e06872ed81d496f204ea80031bbe3dabdd58d1b
uzohou <isaiahntina47@gmail.com>
2024-01-04 17:06:41 +0000

    Add ContactMeSection component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,16 @@ import Hero from "./components/hero";
 import MyLifeStory from "./components/MyLifeStory";
 import VisualPortfolio from "./components/visualportfolio";
 import BodyInfo from "./components/body";
+import ContactMeSection from "./components/reachout";
 
 export default function Home() {
   return (
-    <div>
+    <div className="overflow-hidden" >
       <Hero />
       <MyLifeStory />
       <VisualPortfolio />
       <BodyInfo />
+      <ContactMeSection />
     </div>
   );
 }
