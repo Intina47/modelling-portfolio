8a50b88889df1dad7b76814179419e5c59552e03
uzohou <isaiahntina47@gmail.com>
2024-01-07 08:38:57 +0000

    Refactor import statements in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
-import Hero from "./components/hero";
-import MyLifeStory from "./components/MyLifeStory";
-import VisualPortfolio from "./components/visualportfolio";
-import BodyInfo from "./components/body";
-import ContactMeSection from "./components/reachout";
+import Hero from './components/hero';
+import MyLifeStory from './components/MyLifeStory';
+import VisualPortfolio from './components/visualportfolio';
+import BodyInfo from './components/body';
+import ContactMeSection from './components/reachout';
 
 export default function Home() {
-  return (
-    <div className="overflow-hidden" >
-      <Hero />
-      <MyLifeStory />
-      <VisualPortfolio />
-      <BodyInfo />
-      <ContactMeSection />
-    </div>
-  );
+ return (
+  <div className="overflow-hidden" >
+   <Hero />
+   <MyLifeStory />
+   <VisualPortfolio />
+   <BodyInfo />
+   <ContactMeSection />
+  </div>
+ );
 }
