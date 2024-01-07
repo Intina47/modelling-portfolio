6fe1ea8353ece9ad802a9124e7294603197c9982
uzohou <isaiahntina47@gmail.com>
2024-01-07 08:39:16 +0000

    Fix formatting in hero component

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,22 +1,22 @@
 // components/hero.tsx
 import React from 'react';
-import Navbar from './navbar'
+import Navbar from './navbar';
 
 const Hero = () => {
-  return (
-    <div className="relative h-screen bg-cover bg-center" style={{ backgroundImage: 'url("/hero.jpg")' }}>
-      {/* Transparent Navbar */}
-      <div className="absolute top-0 left-0 right-0 bg-transparent">
-        <Navbar/>
-      </div>
+ return (
+  <div className="relative h-screen bg-cover bg-center" style={{ backgroundImage: 'url("/hero.jpg")' }}>
+   {/* Transparent Navbar */}
+   <div className="absolute top-0 left-0 right-0 bg-transparent">
+    <Navbar/>
+   </div>
 
-      {/* Hero Content */}
-      <div className="absolute inset-0 flex flex-col justify-center items-center text-white">
-        <h1 className="text-5xl font-bold mb-4">Bossette Model Portfolio</h1>
-        <p className="text-lg">Showcasing the beauty within.</p>
-      </div>
-    </div>
-  );
+   {/* Hero Content */}
+   <div className="absolute inset-0 flex flex-col justify-center items-center text-white">
+    <h1 className="text-5xl font-bold mb-4">Bossette Model Portfolio</h1>
+    <p className="text-lg">Showcasing the beauty within.</p>
+   </div>
+  </div>
+ );
 };
 
 export default Hero;
