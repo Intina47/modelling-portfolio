4b1eb82fcaad73b1a5f454b098e81130dc670756
uzohou <isaiahntina47@gmail.com>
2024-01-07 08:24:46 +0000

    Add ContactMe page

---
+++ pages/contact-me.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../app/components/navbar";
+import ContactMePage from "../app/components/ContactMePage";
+import '../app/globals.css'
+const ContactMe = () => {
+  return (
+    <div>
+      <Navbar />
+      <ContactMePage />
+    </div>
+  );
+};
+
+export default ContactMe;
+
