c76ec79e204d12d513388c20fc1375222d36fa80
uzohou <isaiahntina47@gmail.com>
2024-01-07 08:40:04 +0000

    Refactor import statements and formatting in contact-me.js

--- pages/contact-me.js
+++ pages/contact-me.js
@@ -1,14 +1,14 @@
-import React from "react";
-import Navbar from "../app/components/navbar";
-import ContactMePage from "../app/components/ContactMePage";
-import '../app/globals.css'
+import React from 'react';
+import Navbar from '../app/components/navbar';
+import ContactMePage from '../app/components/ContactMePage';
+import '../app/globals.css';
 const ContactMe = () => {
-  return (
-    <div>
-      <Navbar />
-      <ContactMePage />
-    </div>
-  );
+ return (
+  <div>
+   <Navbar />
+   <ContactMePage />
+  </div>
+ );
 };
 
 export default ContactMe;
