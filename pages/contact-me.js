8e0148019785b65d69386b2c0ce9ca5bad85317f
uzohou <isaiahntina47@gmail.com>
2024-01-09 11:38:28 +0000

    Update contact-me page styling

--- pages/contact-me.js
+++ pages/contact-me.js
@@ -4,7 +4,7 @@ import ContactMePage from '../app/components/ContactMePage';
 import '../app/globals.css';
 const ContactMe = () => {
  return (
-  <div className='bg-black h-screen w-full'>
+  <div className='bg-black h-screen w-full' id="contact-me-page">
    <Navbar />
    <ContactMePage />
   </div>
