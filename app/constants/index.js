cf03b6fea108a0e05a84eb07aeab82729426cdb7
uzohou <isaiahntina47@gmail.com>
2024-01-09 05:59:28 +0000

    Refactor image and description arrays in constants file

--- app/constants/index.js
+++ app/constants/index.js
@@ -1,66 +1,66 @@
 //my life pictures
 export const MyLifeStoryImages = [
-    {
-            image:'/mylife1.jpg',
-            description: 'Balenciaga Walk 2020' 
-    },
-    {
-            image: '/mylife2.jpg',
-            description: 'Vogue Photoshoot'
-    },
-    {
-            image: '/mylife3.jpg',
-            description: 'Runway Show Paris'
-    }
+ {
+  image:'/mylife1.jpg',
+  description: 'Balenciaga Walk 2020'
+ },
+ {
+  image: '/mylife2.jpg',
+  description: 'Vogue Photoshoot'
+ },
+ {
+  image: '/mylife3.jpg',
+  description: 'Runway Show Paris'
+ }
 ];
-//visual portfolio images 
+//visual portfolio images
 export const portfolioItems = [
-    {
-     image: '/visual1.jpg',
-     description: 'Iconic runway walk at Paris Fashion Week.'
-    },
-    {
-     image: '/visual2.jpg',
-     description: 'Vogue photoshoot showcasing elegance.'
-    },
-    {
-     image: '/visual3.jpg',
-     description: 'Collaboration with renowned fashion designer.'
-    },
-    {
-     image: '/visual4.jpg',
-     description: 'Balenciaga walk at New York Fashion Week.'
-    },
-    {
-     image: '/visual5.jpg',
-     description: 'Vogue photoshoot showcasing elegance.'
-    },
-    {
-     image: '/visual6.jpg',
-     description: 'Iconic runway walk at Paris Fashion Week.'
-    },
-    {
-     image: '/visual7.jpg',
-     description: 'Iconic runway walk at Paris Fashion Week.'
-    },
-    {
-     image: '/visual8.jpg',
-     description: 'Iconic runway walk at Paris Fashion Week.'
-    },
-    {
-     image: '/visual9.jpg',
-     description: 'Iconic runway walk at Paris Fashion Week.'
-    },
-   ];
+ {
+  image: '/visual1.jpg',
+  description: 'Iconic runway walk at Paris Fashion Week.'
+ },
+ {
+  image: '/visual2.jpg',
+  description: 'Vogue photoshoot showcasing elegance.'
+ },
+ {
+  image: '/visual3.jpg',
+  description: 'Collaboration with renowned fashion designer.'
+ },
+ {
+  image: '/visual4.jpg',
+  description: 'Balenciaga walk at New York Fashion Week.'
+ },
+ {
+  image: '/visual5.jpg',
+  description: 'Vogue photoshoot showcasing elegance.'
+ },
+ {
+  image: '/visual6.jpg',
+  description: 'Iconic runway walk at Paris Fashion Week.'
+ },
+ {
+  image: '/visual7.jpg',
+  description: 'Iconic runway walk at Paris Fashion Week.'
+ },
+ {
+  image: '/visual8.jpg',
+  description: 'Iconic runway walk at Paris Fashion Week.'
+ },
+ {
+  image: '/visual9.jpg',
+  description: 'Iconic runway walk at Paris Fashion Week.'
+ },
+];
 
-   //body measurements
-   export  const bodyInfoItems = [
-    { title: 'Height', value: '5\'10"' },
-    { title: 'Bust', value: '34' },
-    { title: 'Waist', value: '26' },
-    { title: 'Hips', value: '34' },
-    { title: 'Shoes', value: 'US 8' },
-    { title: 'Dress', value: 'US 4' },
-    { title: 'Hair', value: 'Black' },
-    { title: 'Eyes', value: 'Black' },
-   ];
+//body measurements
+export  const bodyInfoItems = [
+ { title: 'Height', value: '5\'10"' },
+ { title: 'Bust', value: '34' },
+ { title: 'Waist', value: '26' },
+ { title: 'Hips', value: '34' },
+ { title: 'Shoes', value: 'US 8' },
+ { title: 'Dress', value: 'US 4' },
+ { title: 'Hair', value: 'Black' },
+ { title: 'Eyes', value: 'Black' },
+];
