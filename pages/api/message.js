8d9152f7dc4aab55b5fdd159e2f29a608f20aad2
uzohou <isaiahntina47@gmail.com>
2024-01-09 11:38:33 +0000

    Refactor API message handling code

--- pages/api/message.js
+++ pages/api/message.js
@@ -5,48 +5,50 @@ import xss from 'xss';
 dotenv.config();
 
 export default function handler(req, res) {
- if(req.method !== 'POST'){
-  res.status(405).json({error: 'method not allowed'});
+ if (req.method !== 'POST') {
+  res.status(405).json({ error: 'Method not allowed' });
   return;
  }
- const {firstName, lastName, email, message} = req.body;
 
- if(!firstName || !lastName || !email || !message){
-  res.status(400).json({error: 'empty field identified'});
+ const { firstName, lastName, email, message } = req.body;
+
+ if (!firstName || !lastName || !email || !message) {
+  res.status(400).json({ error: 'Empty field identified' });
   return;
- }else{
-  const name = firstName + ' ' + lastName;
-  const cleanlastname = xss(lastName);
-  const cleanedname = xss(name);
-  const cleanedemail = xss(email);
-  const cleanedmessage = xss(message);
+ }
 
-  if (cleanedname.includes('suspicious') || cleanedemail.includes('suspicious') || cleanedmessage.includes('suspicious')) {
-   throw new Error('Suspicious content detected');
+ const name = firstName + ' ' + lastName;
+ const cleanLastName = xss(lastName);
+ const cleanedName = xss(name);
+ const cleanedEmail = xss(email);
+ const cleanedMessage = xss(message);
+
+ if (cleanedName.includes('suspicious') || cleanedEmail.includes('suspicious') || cleanedMessage.includes('suspicious')) {
+  res.status(400).json({ error: 'Suspicious content detected' });
+  return;
+ }
+
+ const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+   user: process.env.EMAIL,
+   pass: process.env.PASS
+  },
+ });
+
+ const mailOptions = {
+  from: process.env.EMAIL,
+  to: process.env.EMAIL,
+  subject: `MESSAGE FROM ${cleanLastName.toUpperCase()} VIA YOUR PERSONAL PORTFOLIO`,
+  text: `Name: ${cleanedName}\nEmail: ${cleanedEmail}\nMessage: ${cleanedMessage}`
+ };
+
+ transporter.sendMail(mailOptions, (error) => {
+  if (error) {
+   res.status(500).json({ error: 'Error sending mail' });
+   console.log(error);
+  } else {
+   res.status(200).json({ message: 'Success' });
   }
-
-  const transporter = nodemailer.createTransport({
-   service: 'gmail',
-   auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASS
-   },
-  });
-
-  const mailOptions = {
-   from: process.env.EMAIL,
-   to: process.env.EMAIL,
-   subject: `MESSAGE FROM ${cleanlastname.toUpperCase()} VIA YOUR PERSONAL PORTFOLIO`,
-   text: `Name: ${cleanedname}\nEmail: ${cleanedemail}\nMessage: ${cleanedmessage}`
-  };
-
-  transporter.sendMail(mailOptions, (error) => {
-   if(error){
-    res.status(500).json({error: 'Error sending mail'});
-    console.log(error);
-   }else{
-    res.status(200).json({message: 'success'});
-   }
-  });
- }
+ });
 }
